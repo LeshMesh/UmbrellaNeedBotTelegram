d398269cdabe91679bd6665ed1aebfe3bda1278a
LeshMesh <ivlex37rus@gmail.com>
2022-08-11 20:26:56 +0300

    Refactoring

--- UmbrellaNeedBot.py
+++ UmbrellaNeedBot.py
@@ -1,8 +1,12 @@
-import telebot
-from telebot import types
-from pyowm.commons.exceptions import NotFoundError
+import time
+
 import requests
+import telebot
+from pyowm.commons.exceptions import NotFoundError
+from telebot import types
+
 import maya
+import schedule
 
 token = "5386079328:AAEUFk3RECKKWPw7aeFitBBwEiu-zdsREc4"
 bot = telebot.TeleBot(token)
@@ -12,7 +16,7 @@ user_dict = {}
 markupDefault = types.ReplyKeyboardMarkup(resize_keyboard=True)
 btn1 = types.KeyboardButton('Узнать погоду сейчас')
 btn2 = types.KeyboardButton('Создать ежедневный запрос погоды')
-btnHelp = types.KeyboardButton('Помощь')
+btnHelp = types.InlineKeyboardButton(text='Помощь', callback_data='/help')  # почтитать
 markupDefault.add(btn1, btn2)
 markupDefault.add(btnHelp)
 
@@ -23,7 +27,7 @@ def send_welcome(message):
 
 
 @bot.message_handler(content_types=['text'])
-def echo_all(message):      # переименовать
+def get_task(message):
     if message.text == "Узнать погоду сейчас":
         msg = bot.send_message(message.chat.id, "Напишите Ваш город")
         bot.register_next_step_handler(msg, send_weather)
@@ -31,16 +35,49 @@ def echo_all(message):      # переименовать
         msg = bot.send_message(message.chat.id, "Напишите Ваш город")
         bot.register_next_step_handler(msg, save_city)
     elif message.text == "Помощь":
-        send_welcome(message)       # сделать отдельную функцию help
+        send_welcome(message)  # сделать отдельную функцию help
     else:
         bot.send_message(message.chat.id, "Не понял Вас")
 
 
+def get_geo(message):
+    try:
+        #        city = message.text
+        body = {'q': message, 'limit': 1, 'appid': "d491912f4c590c59b48e8f977324a8e2"}
+        r = requests.get("http://api.openweathermap.org/geo/1.0/direct", params=body)
+        r_dict = r.json()[0]
+        lat = r_dict['lat']
+        lon = r_dict['lon']
+        rez_city = r_dict['local_names']['ru']
+        return lat, lon, rez_city
+    except NotFoundError:  # написать другое исключение
+        answer = bot.send_message(message.chat.id, f'Не могу найти такой город. Попробуйте еще раз:')
+        bot.register_next_step_handler(answer, get_geo, reply_markup=markupDefault)
+
+
+def get_weather(lat, lon):
+    body = {'lat': lat, 'lon': lon}
+    header = {'X-Yandex-API-Key': "dfc4c800-e690-42c6-be72-9113abcdbeca"}
+    r = requests.get("https://api.weather.yandex.ru/v2/informers", params=body, headers=header)
+    now_temp = str(r.json()['fact']['temp'])
+    return now_temp
+
+
+def send_weather(message):
+    city = message.text
+    lat, lon, rez_city = get_geo(city)
+    now_temp = get_weather(lat, lon)
+    answer = "По данным Яндекс.Погоды, в городе " + rez_city + " сейчас " + now_temp
+    bot.send_message(message.chat.id, answer, reply_markup=markupDefault)
+
+
 @bot.message_handler(content_types=['text'])
 def save_city(message):
-    city = message.text
-    lat, lon, rez_city = get_geo(message)
+    lat, lon, rez_city = get_geo(message.text)
     user_dict["city"] = rez_city
+    user_dict["lat"] = lat
+    user_dict["lon"] = lon
+    user_dict["chat_id"] = message.chat.id
     msg = bot.send_message(message.chat.id, "Напишите время уведомления по МСК")
     bot.register_next_step_handler(msg, save_time)
 
@@ -56,13 +93,14 @@ def save_time(message):
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, "Создать уведомление:", reply_markup=markup)
     msg = bot.send_message(message.chat.id, user_dict["city"] + " в " + user_dict["time"] + "?")
-    bot.register_next_step_handler(msg, echo_all2)
+    bot.register_next_step_handler(msg, save_notification)
 
 
 @bot.message_handler(content_types=['text'])
-def echo_all2(message):      # переименовать
+def save_notification(message):
     if message.text == "Да":
-        notificate(message)
+        bot.send_message(message.chat.id, "Ок", reply_markup=markupDefault)
+        send_notification()
     elif message.text == "Нет, изменить":
         msg = bot.send_message(message.chat.id, "Напишите Ваш город")
         bot.register_next_step_handler(msg, save_city)
@@ -70,38 +108,20 @@ def echo_all2(message):      # переименовать
         bot.send_message(message.chat.id, "Не понял Вас")
 
 
-def notificate(message):
-    bot.send_message(message.chat.id, "Ок", reply_markup=markupDefault)
-
-
-def get_geo(message):
-    try:
-        city = message.text
-        body = {'q': city, 'limit': 1, 'appid': "d491912f4c590c59b48e8f977324a8e2"}
-        r = requests.get("http://api.openweathermap.org/geo/1.0/direct", params=body)
-        r_dict = r.json()[0]
-        lat = r_dict['lat']
-        lon = r_dict['lon']
-        rez_city = r_dict['local_names']['ru']
-        return lat, lon, rez_city
-    except NotFoundError:       # написать другое исключение
-        answer = bot.send_message(message.chat.id, f'Не могу найти такой город. Попробуйте еще раз:')
-        bot.register_next_step_handler(answer, get_geo, reply_markup=markupDefault)
-
-
-def get_weather(lat, lon):
-    body = {'lat': lat, 'lon': lon}
-    header = {'X-Yandex-API-Key': "dfc4c800-e690-42c6-be72-9113abcdbeca"}
-    r = requests.get("https://api.weather.yandex.ru/v2/informers", params=body, headers=header)
-    now_temp = str(r.json()['fact']['temp'])
-    return now_temp
-
-
-def send_weather(message):
-    lat, lon, rez_city = get_geo(message)
+def send_notification():
+    city = user_dict["city"]
+    time_nf = user_dict["time"]
+    lat, lon, rez_city = get_geo(city)
     now_temp = get_weather(lat, lon)
-    answer = "По данным Яндекс.Погоды, в городе " + rez_city + " сейчас " + now_temp
-    bot.send_message(message.chat.id, answer, reply_markup=markupDefault)
+    answer = "По данным Яндекс.Погоды, в городе " + city + " сейчас " + now_temp + " " + time_nf
+    bot.send_message(user_dict["chat_id"], answer, reply_markup=markupDefault)
 
+
+# schedule.every(2).minutes.do(send_notification)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
 bot.infinity_polling(none_stop=True)
+
